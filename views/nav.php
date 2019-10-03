2dba1165d778410cf2fd3a584a1a24d1858a960f
Maud <s1146976@student.windesheim.nl>
2019-10-03 13:58:41 +0200

    indexpage

--- views/nav.php
+++ views/nav.php
@@ -1,1 +1,8 @@
-<?php
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm">
+    <a class="my-0 mr-md-auto" href="index.php"><img class="" src="Resources/Images/logotaalproofsmal.fw.png" alt=""></a>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Video's</a>
+        <a class="p-2 text-dark" href="#">Account</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Log in</a>
+</div>
