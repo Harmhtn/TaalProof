02204dfca8924e54ec83fb5bb0fd6965a9d3a44e
Maud <s1146976@student.windesheim.nl>
2019-10-03 12:30:15 +0200

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'server_name' => "remotemysql.com",
+    'username' => "ti6l6GEhzS",
+    'password' => "2qFRB9dcoy",
+    'db_name' => "ti6l6GEhzS"
+];
---
+++ src/Classes/QueryMyDatabase.php
@@ -0,0 +1,20 @@
+<?php
+
+require "Connection.php";
+
+class QueryMyDatabase extends Connection
+{
+//    public function getAllProduct()
+//    {
+//        $sql = ("SELECT *
+//                FROM customers");
+//
+//        $sel = $this->conn->prepare($sql);
+//
+//        $sel->execute();
+//
+//        $results = $sel->fetchAll();
+//
+//        return $results;
+//    }
+}
